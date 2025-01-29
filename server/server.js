648f3c5402ad3d02d0a38ddec327ad2275ea097f
andrea_merlos <merlosandrea13@gmail.com>
2025-01-28 19:40:12 -0500

    OH . NO

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import mysql from "mysql2";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: "GET,POST,PUT,DELETE",
+//     credentials: true,
+//   })
+// );
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -20,8 +22,15 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
+// Resolve the path to the 'dist' directory
+const distPath = path.resolve(__dirname, "../../dist");
+
+// Serve static files from the 'dist' directory
+app.use(express.static(distPath));
+
+// Example route
 app.get("/", (req, res) => {
-  return res.json("From backend");
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
 app.get("/products", (req, res) => {
@@ -32,7 +41,7 @@ app.get("/products", (req, res) => {
   });
 });
 
-app.listen(8081, () => {
+app.listen(80, () => {
   console.log("listening");
 });
 
